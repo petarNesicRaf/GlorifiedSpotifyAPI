a1f362c252fa9b0b38edd0fee577132130dacf2a
petar <pnesic6219rn@raf.rs>
2023-10-08 21:59:44 +0200

    Remote source for track analysis.

--- src/main/java/com/glorified/spotifygapi/resources/remote/RemoteTrackResource.java
+++ src/main/java/com/glorified/spotifygapi/resources/remote/RemoteTrackResource.java
@@ -3,6 +3,7 @@ package com.glorified.spotifygapi.resources.remote;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.glorified.spotifygapi.models.track.Track;
+import com.glorified.spotifygapi.models.track.TrackFeatures;
 import com.glorified.spotifygapi.resources.AuthenticationResource;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
@@ -14,6 +15,7 @@ import org.apache.http.util.EntityUtils;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
@@ -25,8 +27,7 @@ public class RemoteTrackResource {
 
     @GET
     @Path("/playlist/{id}")
-    public Response fetchTracksFromPlaylist(@PathParam("id") String id)
-    {
+    public Response fetchTracksFromPlaylist(@PathParam("id") String id) {
         HttpClient httpClient = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet("https://api.spotify.com/v1/playlists/" + id + "/tracks");
         httpGet.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + AuthenticationResource.tokenManager.getToken().getAccessToken());
@@ -35,14 +36,13 @@ public class RemoteTrackResource {
             HttpResponse httpResponse = httpClient.execute(httpGet);
             if (httpResponse.getStatusLine().getStatusCode() == 200) {
                 String responseBody = EntityUtils.toString(httpResponse.getEntity());
-                ObjectMapper objectMapper =new ObjectMapper();
+                ObjectMapper objectMapper = new ObjectMapper();
                 JsonNode tree = objectMapper.readTree(responseBody);
                 JsonNode items = tree.get("items");
 
                 List<Track> tracks = new ArrayList<>();
 
-                for(JsonNode item:items)
-                {
+                for (JsonNode item : items) {
                     Track track = deserializeTracks(item);
                     tracks.add(track);
                 }
@@ -56,6 +56,48 @@ public class RemoteTrackResource {
             throw new RuntimeException(e);
         }
     }
+
+    //analysis?id=kdakdak&id=jadjdaj
+    @GET
+    @Path("/analysis")
+    public Response fetchTracksAnalysis(@QueryParam("id") List<String> ids)
+    {
+        String list = "ids=";
+        for(String s:ids)
+        {
+
+            list += s;
+            list +=",";
+        }
+        list = list.substring(0, list.length()-1);
+        HttpClient httpClient = HttpClients.createDefault();
+
+        HttpGet httpGet = new HttpGet("https://api.spotify.com/v1/audio-features?"+list);
+        httpGet.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + AuthenticationResource.tokenManager.getToken().getAccessToken());
+
+        try {
+            HttpResponse response = httpClient.execute(httpGet);
+            String responseBody = EntityUtils.toString(response.getEntity());
+            if (response.getStatusLine().getStatusCode() == 200)
+            {
+               // List<TrackFeatures> features = trackService.insertTrackFeatures(responseBody, ids);
+                List<TrackFeatures> tf = new ArrayList<>();
+                ObjectMapper objectMapper= new ObjectMapper();
+                JsonNode features = objectMapper.readTree(responseBody).get("audio_features");
+
+                for (JsonNode feature : features) {
+                    TrackFeatures track = deserializeFeature(feature);
+                    tf.add(track);
+                }
+                return Response.ok(tf, MediaType.APPLICATION_JSON).build();
+            }else{
+                return Response.status(response.getStatusLine().getStatusCode()).entity(responseBody).build();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private Track deserializeTracks(JsonNode item)
     {
         Track track = new Track();
@@ -121,6 +163,27 @@ public class RemoteTrackResource {
         } else {
             return "null"; // Or set a default URL or handle it as needed
         }
+    }
+
+    private TrackFeatures deserializeFeature(JsonNode feature)
+    {
+        TrackFeatures trackFeatures = new TrackFeatures();
+        trackFeatures.setTrackID("null");
+        trackFeatures.setAnalysisID(feature.get("id").asText());
+        trackFeatures.setAccousticness(feature.get("acousticness").asDouble());
+        trackFeatures.setDancebility(feature.get("danceability").asDouble());
+        trackFeatures.setEnergy(feature.get("energy").asDouble());
+        trackFeatures.setInstrumentalness(feature.get("instrumentalness").asDouble());
+        trackFeatures.setKey(feature.get("key").asDouble());
+        trackFeatures.setLiveness(feature.get("liveness").asDouble());
+        trackFeatures.setLoudness(feature.get("loudness").asDouble());
+        trackFeatures.setModality(feature.get("mode").asDouble());
+        trackFeatures.setSpeechiness(feature.get("speechiness").asDouble());
+        trackFeatures.setBpm(feature.get("tempo").asDouble());
+        trackFeatures.setTimeSignature(feature.get("time_signature").asDouble());
+        trackFeatures.setValence(feature.get("valence").asDouble());
+        trackFeatures.setHref(feature.get("track_href").asText());
+        return trackFeatures;
     }
 
 
